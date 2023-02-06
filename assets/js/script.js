50666e3267659ba508b94c99e1bcce039a550c8b
Joseph <117396146+joeytraffic@users.noreply.github.com>
2023-02-06 00:27:48 -0700

    started js and added button in html

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+
+
+
+var apiURL = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=3a0745b6c5398a233987de0276531a08";
+var APIKey = "3a0745b6c5398a233987de0276531a08"
+
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + currentCity + "&appid=" + APIKey;
+
